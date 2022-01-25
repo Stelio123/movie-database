0a7510021139d71260c03118dcadb81195680645
Stelianos Langaditis <slangaditis96@gmail.com>
2022-01-24 21:22:37 -0800

    mobile css done for about page and home pages

--- src/pages/PageAbout.js
+++ src/pages/PageAbout.js
@@ -15,18 +15,9 @@ function PageAbout() {
 			<section className="about-section">
 				<h2>Welcome to SNOS MOVIES!</h2>
 				<p>SNOS MOVIES is an online database related to all movies old and new.</p>
-			</section>
-
-			<section className="tmdb-section">
 				<p>This Database uses the TMDb API but is in no way endorsed or supported by TMDb. This website was created for educational purposes only.</p>
-			</section>
-
-			<div className="education-note">
-            <p>For Educational Purposes Only</p>
-            <p>&copy; {getYear()} Stelianos Langaditis</p>
-        </div>
-
-			
+            	<p>&copy; {getYear()} Stelianos Langaditis</p>
+			</section>	
 		</main>
 	);
 }
