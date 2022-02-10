e9a4b56d7b6c66b9d3fa6579a9d4301f20cec0e0
Stelianos Langaditis <slangaditis96@gmail.com>
2022-02-10 11:28:56 -0800

    yes

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import favReducer from '../features/FavSlice';
-
-export const store = configureStore({
-    reducer: {
-        favs: favReducer 
-    }
-});
--- src/utilities/utilities.js
+++
@@ -1,7 +0,0 @@
-// Utilities
-function getYear(){
-    const d = new Date();
-    return d.getFullYear();
-}
-
-export { getYear }
